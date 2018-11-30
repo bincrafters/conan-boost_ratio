3060f09a114eb88b03e3222047e96715a5583477
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostRatioConan(base.BoostBaseConan):
     name = "boost_ratio"
@@ -20,5 +20,3 @@ class BoostRatioConan(base.BoostBaseConan):
         "boost_static_assert",
         "boost_type_traits"
     ]
-
-
