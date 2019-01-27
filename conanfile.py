9ed8e7fb6c30271db12a6979b3dac2c5fb21c88b
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:58:41 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostRatioConan(base.BoostBaseConan):
     name = "boost_ratio"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_ratio"
     lib_short_names = ["ratio"]
     header_only_libs = ["ratio"]
