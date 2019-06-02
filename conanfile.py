b427573f25883d4b3bcd7be4826ea7d498ae9587
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:07 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,20 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostRatioConan(base.BoostBaseConan):
     name = "boost_ratio"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_ratio"
-    lib_short_names = ["ratio"]
-    header_only_libs = ["ratio"]
-    b2_requires = [
-        "boost_config",
-        "boost_core",
-        "boost_integer",
-        "boost_mpl",
-        "boost_rational",
-        "boost_static_assert",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
